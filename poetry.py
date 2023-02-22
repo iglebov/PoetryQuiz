fbb279db2e3d2478ed2e96123ea7085d72b42dfa
iglebov <iglebov40@gmail.com>
2023-02-22 23:04:33 +0300

    First files upload.

---
+++ poetry.py
@@ -0,0 +1,26 @@
+# Стихотворения
+poetry_dict = {
+    "название_вашего_стихотворения": [
+        "Первая строка стихотворения,",
+        "Вторая строка стихотворения.",
+    ],
+    "Рад тебя видеть": [
+        "Всегда,",
+        "Что-то происходит.",
+        "Взлёты и падения,",
+        "Когда-то проходят.",
+        "Возможно научиться их ценить,",
+        "Учит наша короткая жизнь.",
+    ],
+    "Dark night": [
+        "Yeah, I guess I’m alright,",
+        "Writing some notes,",
+        "To get rid of my thoughts,",
+        "The past and future all combined,",
+        "Been heavy lately on my mind,",
+        "I am sure it’s temporary,",
+        "No need more worries,",
+        "The good side actually,",
+        "I will be able to tell more stories.",
+    ],
+}
